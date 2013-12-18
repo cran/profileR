fcd3ae32129ca33878ec5a87820f268447c0231c
Christopher David Desjardins <cddesjardins@gmail.com>
2013-12-18 15:32:14 +0000

    version 0.2

--- R/summary.critpat.R
+++ R/summary.critpat.R
@@ -4,7 +4,15 @@
 summary.critpat <- function(object, ...){
 	cat("Call:\n")
 	print(object$call)
-	cat("\nHypothesis Tests:\n")
-	cat("\n")
-	print(object$F.table)
+  cat("\nRelability\n")
+	print(object$r2)
+  
+  if(is.null(object$lvl.comp) == FALSE){
+  cat("\n Level Component\n")
+  print(object$lvl.comp)}
+  if(is.null(object$pat.comp) == FALSE){
+  cat("\n Pattern Component \n")
+  print(object$pat.comp)
+  }
 }
+
